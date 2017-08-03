2f202cb0dfc0409ac3eb86ef1bf337dbb38a3027
Isaac Norman <isaacdnorman@gmail.com>
2017-08-03 10:49:41 +1000

    Add session model and migration generator

--- lib/devise_sessionable/engine.rb
+++ lib/devise_sessionable/engine.rb
@@ -3,10 +3,10 @@ module DeviseSessionable
     isolate_namespace DeviseSessionable
 
     config.generators do |g|
-     g.test_framework :rspec, :fixture => false
-     g.fixture_replacement :factory_girl, :dir => 'spec/factories'
-     g.assets false
-     g.helper false
-     end
+      g.test_framework :rspec, :fixture => false
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.assets false
+      g.helper false
+    end
   end
 end
